b8518b30446d93976d9476295b6c03a4799659e6
Emily Gagne <ceg9498@gmail.com>
2019-09-24 09:03:47 -0400

    Indent a line.

--- src/component/options.tsx
+++ src/component/options.tsx
@@ -24,7 +24,7 @@ export default function Options(props:IOptionsProps){
       <br/>
       <FormControlLabel control={
         <Switch 
-        checked={optionsState.viewSlim} 
+          checked={optionsState.viewSlim} 
           onChange={(e)=>setOptions(e,"viewSlim")} />
       } label="Slim Timers" />
     </>
